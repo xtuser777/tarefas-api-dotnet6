a34bbb365029752cab3df5ca4c6d0610eadb5255
Lucas Oliveira <lucaoxt@gmail.com>
2023-11-30 16:10:34 -0300

    post, put and delete endpoints

--- Endpoints/TarefasEndpoints.cs
+++ Endpoints/TarefasEndpoints.cs
@@ -17,5 +17,36 @@ public static class TarefasEndpoints
             if (tarefas is null) return Results.NotFound();
             return Results.Ok(tarefas);
         });
+
+        app.MapGet("/tarefas/{id}", async (GetConnection getConnection, int id) =>
+        {
+            using var conn = await getConnection();
+            var tarefa = conn.Get<Tarefa>(id);
+            if (tarefa is null) return Results.NotFound();
+            return Results.Ok(tarefa);
+        });
+
+        app.MapPost("/tarefas", async (GetConnection getConnection, Tarefa Tarefa) =>
+        {
+            using var conn = await getConnection();
+            var id = conn.Insert(Tarefa);
+            return Results.Created($"/tarefas/{id}", Tarefa);
+        });
+
+        app.MapPut("/tarefas", async (GetConnection getConnection, Tarefa Tarefa) =>
+        {
+            using var conn = await getConnection();
+            var id = conn.Update(Tarefa);
+            return Results.Ok();
+        });
+
+        app.MapDelete("/tarefas/{id}", async (GetConnection getConnection, int id) =>
+        {
+            using var conn = await getConnection();
+            var tarefa = conn.Get<Tarefa>(id);
+            if (tarefa is null) return Results.NotFound();
+            conn.Delete(tarefa);
+            return Results.Ok(tarefa);
+        });
     }
 }
